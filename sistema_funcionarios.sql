e23e81e8bb206813d790f64204c5f747cff919fd
GustavoGG2807 <gg.contadescarte@gmail.com>
2025-11-01 12:18:11 -0300

    Criando tabelas clientes, pedidos e itens_pedido no PHPmyadmin

--- sistema_funcionarios.sql
+++ sistema_funcionarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/09/2025 às 16:52
+-- Tempo de geração: 01/11/2025 às 16:15
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `sistema_funcionarios`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `idCli` int(11) NOT NULL,
+  `nome_completo` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `endereco` varchar(150) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `clientes`
+--
+
+INSERT INTO `clientes` (`idCli`, `nome_completo`, `email`, `usuario`, `senha`, `endereco`, `telefone`, `data_cadastro`) VALUES
+(1, 'Gustavo', 'gustavo@grano.com', 'GG', '$2y$10$eCrP2yojbGvbgvYfHNbPm.RvAM8BBjeP9GEH2/yzm5nScvLmIxpKi', 'Rua Tutóia', '12112313231331213', '2025-10-29 21:01:32'),
+(3, 'cliente um', 'clienteum@grano.com', 'Cliente 1', '$2y$10$VBgngy9KAVb3wZC.EbinY.FRY69iSYSsweyl2F1TPBj2asFoVAbW.', 'Rua Cliente', '32413144114124', '2025-10-29 21:12:29');
 
 -- --------------------------------------------------------
 
@@ -51,6 +76,53 @@ INSERT INTO `funcionarios` (`idFunc`, `usuario`, `senha`, `nome_completo`, `emai
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `itens_pedido`
+--
+
+CREATE TABLE `itens_pedido` (
+  `idItem` int(11) NOT NULL,
+  `idPedido` int(11) NOT NULL,
+  `idProd` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `preco_unitario` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `itens_pedido`
+--
+
+INSERT INTO `itens_pedido` (`idItem`, `idPedido`, `idProd`, `quantidade`, `preco_unitario`) VALUES
+(1, 3, 43, 3, 5.00),
+(2, 3, 45, 1, 9.90),
+(3, 4, 1, 1, 10.00),
+(4, 5, 1, 1, 10.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `idPedido` int(11) NOT NULL,
+  `idCli` int(11) NOT NULL,
+  `data_pedido` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('Pendente','Em preparação','Saiu para entrega','Concluído') DEFAULT 'Pendente',
+  `total` decimal(10,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `pedidos`
+--
+
+INSERT INTO `pedidos` (`idPedido`, `idCli`, `data_pedido`, `status`, `total`) VALUES
+(3, 3, '2025-10-29 03:00:00', 'Pendente', 0.00),
+(4, 3, '2025-10-29 03:00:00', 'Pendente', 0.00),
+(5, 3, '2025-10-29 03:00:00', 'Pendente', 0.00);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `produtos`
 --
 
@@ -68,10 +140,10 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`idProd`, `nome`, `categoria`, `preco`, `quantidade`, `fotos`) VALUES
-(1, 'Brioche', 'Pães Doces', 10.00, 22, 'imagens/brioche.jpg'),
-(43, 'Donuts', 'Doces', 5.00, 10, 'imagens/68b18ebe8abeb.jpg'),
+(1, 'Brioche', 'Pães Doces', 10.00, 20, 'imagens/brioche.jpg'),
+(43, 'Donuts', 'Doces', 5.00, 7, 'imagens/68b18ebe8abeb.jpg'),
 (44, 'Pão Francês', 'Pães', 0.80, 500, 'imagens/68d004f4e6bc6.jpg'),
-(45, 'Pão de Forma Integral', 'Pães', 9.90, 40, 'imagens/68d007222c3dd.jpg'),
+(45, 'Pão de Forma Integral', 'Pães', 9.90, 39, 'imagens/68d007222c3dd.jpg'),
 (46, 'Pão de Batata com Recheio de Catupiry', 'Pães', 5.50, 60, 'imagens/68d00749c441a.jpg'),
 (47, 'Baguete Tradicional', 'Pães', 7.00, 35, 'imagens/68d0076373c68.jpg'),
 (48, 'Pão Doce com Coco', 'Pães Doces', 3.50, 70, 'imagens/68d0077f845a6.jpg'),
@@ -97,10 +169,33 @@ INSERT INTO `produtos` (`idProd`, `nome`, `categoria`, `preco`, `quantidade`, `f
 --
 
 --
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idCli`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
 -- Índices de tabela `funcionarios`
 --
 ALTER TABLE `funcionarios`
   ADD PRIMARY KEY (`idFunc`);
+
+--
+-- Índices de tabela `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  ADD PRIMARY KEY (`idItem`),
+  ADD KEY `idPedido` (`idPedido`),
+  ADD KEY `idProd` (`idProd`);
+
+--
+-- Índices de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `idCli` (`idCli`);
 
 --
 -- Índices de tabela `produtos`
@@ -113,16 +208,51 @@ ALTER TABLE `produtos`
 --
 
 --
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idCli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de tabela `funcionarios`
 --
 ALTER TABLE `funcionarios`
   MODIFY `idFunc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT de tabela `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  MODIFY `idItem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
   MODIFY `idProd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  ADD CONSTRAINT `itens_pedido_ibfk_1` FOREIGN KEY (`idPedido`) REFERENCES `pedidos` (`idPedido`),
+  ADD CONSTRAINT `itens_pedido_ibfk_2` FOREIGN KEY (`idProd`) REFERENCES `produtos` (`idProd`);
+
+--
+-- Restrições para tabelas `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`idCli`) REFERENCES `clientes` (`idCli`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
